5afbffd92297a0a25eec9a47b2f44cf27ec55e26
Julie Torres <reeddetorres@gmail.com>
2015-01-15 19:39:14 -0500

    Add numbers exercises: Enumerable collect & count methods

---
+++ Numbers.rb
@@ -0,0 +1,24 @@
+class NumberTransformer
+  include Enumerable
+  
+  def self.multiply_by_ten(array)
+    array.collect { |x| x * 10 }
+  end
+
+  def self.divisible_by_3(array)
+    i = 0
+    booleans = array.collect{ |x| x % 3 == 0 }
+    booleans.collect{ |bool| i += 1 if bool == true }
+    return i
+  end
+  
+  def self.count_divisible_by_3(array)
+    array.count { |x| x % 3 == 0 }
+  end
+end
+
+puts NumberTransformer.divisible_by_3((1..12).to_a) 
+puts NumberTransformer.multiply_by_ten((1..9).to_a)
+puts NumberTransformer.count_divisible_by_3((1..12).to_a)
+puts NumberTransformer.divisible_by_3((1..12).to_a) == NumberTransformer.count_divisible_by_3((1..12).to_a)
+
