87ef40bf1abe03bebd5211c40004c01b3a8bdd56
Julie Torres <reeddetorres@gmail.com>
2015-02-22 11:12:10 -0500

    Corrections on srand method

--- random.rb
+++ random.rb
@@ -1,41 +1,30 @@
 #Setting 4 variables to empty arrays
-stored_values, unseeded_values, seeded_values, seeded_values_again = [], [], [], []
+unseeded_values, seeded_values, seeded_values_again = [], [], [] 
 
-#Creating and storing a list of random numbers
-srand(1234)
-for i in 1..10
-  stored_values << rand(10)
-end
-
-#Creating a list of random numbers without referencing the stored list
-for i in 1..10
-  unseeded_values << rand(10)
-end
-
-#Using the stored list to create a list of random numbers that matches the previous list
+#Creating a list of random numbers based on given seed number 
 srand(1234)
 for i in 1..10
   seeded_values << rand(10)
 end
 
-#Doing it again to illustrate that the list can be used multiple times
+#Creating a list of random numbers without providing a seed number 
+for i in 1..10
+  unseeded_values << rand(10)
+end
+
+#Creating the same list of "random" values by using the same seed number
 srand(1234)
 for i in 1..10
   seeded_values_again << rand(10)
 end
 
 
-puts "Storing list of random numbers:"
-puts stored_values.join(", ")
+puts "A list of values based on the seed number: "
+puts seeded_values.join(", ")
 
 puts "A list of random numbers without calling the seeded values:"
 puts unseeded_values.join(", ")
 
-puts "Calling the seeded list of random values:"
-puts seeded_values.join(", ")
-
-puts "And calling them again:"
+puts "Using the same seed number"
 puts seeded_values_again.join(", ")
 
-puts "srand(1234) stores the value and calling srand(1234) again ensures you get the same values" if stored_values == seeded_values
-puts "Seeded values can be called as many times as needed" if stored_values == seeded_values_again
