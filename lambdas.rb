44695af823e5a745925a3e8bc0f8d4e9d9e694c8
Julie Torres <reeddetorres@gmail.com>
2015-02-23 21:17:37 -0500

    More exercises with lambdas

--- lambdas.rb
+++ lambdas.rb
@@ -7,5 +7,21 @@ end
 #nums.select { |num| num % 2 == 0 }
 
 even = lambda { |num| num % 2 == 0 }
+odd = lambda { |num| num % 2 != 0 }
+divisible_by_3 = lambda { |num| num % 3 == 0 }
+divisible_by_5 = lambda { |num| num % 5 == 0 }
 
-puts nums.select(&even).join(", ")
+rand = rand(1..5)
+if divisible_by_5.call(rand)
+  puts "Multiples of 5: "
+  puts nums.select(&divisible_by_5).join(", ")
+elsif divisible_by_3.call(rand)
+  puts "Multiples of 3: "
+  puts nums.select(&divisible_by_3).join(", ")
+elsif even.call(rand)
+  puts "Even numbers:"
+  puts nums.select(&even).join(", ")
+else
+  puts "Odd numbers:"
+  puts nums.select(&odd).join(", ")
+end
